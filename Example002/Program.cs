1b93b4e212b02beaac13fa43e568538d061701a3
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-06 14:27:45 +0300

    Add ex002

---
+++ Example002/Program.cs
@@ -0,0 +1,16 @@
+﻿// на ввод 3х значное число, 
+//возводим 2ую цифру числа в степень == 3ей цифре числа 
+
+int number= 552;
+int first = (number / 10) % 10;
+int second = number % 10;
+int degree = 1;
+int result = first;
+
+while(degree < second)
+{
+    result = result * first;
+    degree++;
+}
+Console.WriteLine(first);
+Console.WriteLine(result);
